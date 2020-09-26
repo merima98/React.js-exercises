6b22fc01b3d3ae8685fbf286cfd7637cb0417f20
merima98 <merimaceranic98@gmail.com>
2020-09-26 20:26:01 +0200

    Uploading images into application

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -109,7 +109,10 @@ class Feed extends Component {
     this.setState({
       editLoading: true
     });
-    // Set up data (with image!)
+    const formData = new FormData();
+    formData.append('title', postData.title);
+    formData.append('content', postData.content);
+    formData.append('image', postData.image);
     let url = 'http://' + host + ':' + port + '/feed/post';
     let method = 'POST';
     if (this.state.editPost) {
@@ -118,13 +121,7 @@ class Feed extends Component {
 
     fetch(url, {
       method: method,
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        title: postData.title,
-        content: postData.content
-      })
+      body: formData
     })
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {
