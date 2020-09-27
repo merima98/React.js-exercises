f09aa3adc877d61b10747a1b6027851013ee5d97
merima98 <merimaceranic98@gmail.com>
2020-09-27 23:24:49 +0200

    Updating posts

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -63,7 +63,12 @@ class Feed extends Component {
       })
       .then(resData => {
         this.setState({
-          posts: resData.posts,
+          posts: resData.posts.map(post => {
+            return {
+              ...post,
+              imagePath: post.imageUrl
+            };
+          }),
           totalPosts: resData.totalItems,
           postsLoading: false
         });
@@ -116,7 +121,8 @@ class Feed extends Component {
     let url = 'http://' + host + ':' + port + '/feed/post';
     let method = 'POST';
     if (this.state.editPost) {
-      url = 'URL';
+      url = 'http://' + host + ':' + port + '/feed/post/' + this.state.editPost._id;
+      method = 'PUT'
     }
 
     fetch(url, {
