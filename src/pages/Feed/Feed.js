953c335f0eb8510e2ae9255cd3f1fed2984b4890
merima98 <merimaceranic98@gmail.com>
2020-09-28 00:22:25 +0200

    Deleting posts

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -179,7 +179,9 @@ class Feed extends Component {
 
   deletePostHandler = postId => {
     this.setState({ postsLoading: true });
-    fetch('URL')
+    fetch('http://' + host + ':' + port + '/feed/post/' + postId, {
+      method: 'DELETE'
+    })
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {
           throw new Error('Deleting a post failed!');
