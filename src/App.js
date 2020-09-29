3cc586a650c638173290eef9cd1c5887a4fc026a
merima98 <merimaceranic98@gmail.com>
2020-09-29 13:37:27 +0200

    Adding a User Model | Adding user signup validation | Signin users up | Logging in and creating JSON web token

--- src/App.js
+++ src/App.js
@@ -13,11 +13,15 @@ import LoginPage from './pages/Auth/Login';
 import SignupPage from './pages/Auth/Signup';
 import './App.css';
 
+
+const port = process.env.REACT_APP_PORT_NODE_JS;
+const host = process.env.REACT_APP_HOST;
+
 class App extends Component {
   state = {
     showBackdrop: false,
     showMobileNav: false,
-    isAuth: true,
+    isAuth: false,
     token: null,
     userId: null,
     authLoading: false,
@@ -59,7 +63,16 @@ class App extends Component {
   loginHandler = (event, authData) => {
     event.preventDefault();
     this.setState({ authLoading: true });
-    fetch('URL')
+    fetch('http://' + host + ':' + port + '/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: authData.email,
+        password: authData.password
+      })
+    })
       .then(res => {
         if (res.status === 422) {
           throw new Error('Validation failed.');
@@ -100,7 +113,17 @@ class App extends Component {
   signupHandler = (event, authData) => {
     event.preventDefault();
     this.setState({ authLoading: true });
-    fetch('URL')
+    fetch('http://' + host + ':' + port + '/auth/signup', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: authData.signupForm.email.value,
+        password: authData.signupForm.password.value,
+        name: authData.signupForm.name.value
+      })
+    })
       .then(res => {
         if (res.status === 422) {
           throw new Error(
