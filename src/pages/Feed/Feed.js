ded41a5c5b9879e49af16a9df160a912d7008b22
merima98 <merimaceranic98@gmail.com>
2020-10-02 14:29:14 +0200

    Fixing a Bug - The missing username

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -43,8 +43,31 @@ class Feed extends Component {
       .catch(this.catchError);
 
     this.loadPosts();
-    openSocket('http://' + host + ':' + port);
+    const socket = openSocket('http://' + host + ':' + port);
+    socket.on('posts', data => {
+      if (data.action === 'create') {
+        this.addPost(data.post);
+      }
+    });
+
   }
+
+  addPost = post => {
+    this.setState(prevState => {
+      const updatedPosts = [...prevState.posts];
+      if (prevState.postPage === 1) {
+        if (prevState.posts.length >= 2) {
+          updatedPosts.pop();
+        }
+        updatedPosts.unshift(post);
+      }
+      return {
+        posts: updatedPosts,
+        totalPosts: prevState.totalPosts + 1
+      };
+    });
+
+  };
 
   loadPosts = direction => {
     if (direction) {
@@ -174,8 +197,6 @@ class Feed extends Component {
               p => p._id === prevState.editPost._id
             );
             updatedPosts[postIndex] = post;
-          } else if (prevState.posts.length < 2) {
-            updatedPosts = prevState.posts.concat(post);
           }
           return {
             posts: updatedPosts,
